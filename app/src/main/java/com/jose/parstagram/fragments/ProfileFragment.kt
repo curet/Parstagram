ca7c107a74a8e0575ca04f52ddf59df0dbcc6f32
curet <jcscuret@gmail.com>
2022-03-30 00:02:21 -0400

    Profile view appear when clicking Profile

---
+++ app/src/main/java/com/jose/parstagram/fragments/ProfileFragment.kt
@@ -0,0 +1,38 @@
+package com.jose.parstagram.fragments
+
+import android.util.Log
+import com.jose.parstagram.Post
+import com.parse.FindCallback
+import com.parse.ParseException
+import com.parse.ParseQuery
+import com.parse.ParseUser
+
+class ProfileFragment: FeedFragment() {
+    override fun queryPosts(){
+        val query: ParseQuery<Post> = ParseQuery.getQuery(Post::class.java)
+        query.include(Post.KEY_USER)
+        query.whereEqualTo(Post.KEY_USER, ParseUser.getCurrentUser())
+        query.addDescendingOrder("createdAt") // return post in descending order: newer posts first
+        query.findInBackground(object: FindCallback<Post> {
+            // Find all the posts from the server
+            override fun done(posts: MutableList<Post>?, e: ParseException?) {
+                if (e != null){
+                    // Something wrong
+                    Log.e(TAG, "Error fetching posts")
+                }else{
+                    if (posts != null){
+                        for (post in posts){
+                            Log.i(TAG, "Post: " + post.getDescription() + " , username: "
+                                    + post.getUser()?.username)
+                        }
+
+                        allPosts.addAll(posts)
+                        adapter.notifyDataSetChanged()
+                    }
+                }
+            }
+
+        })
+    }
+
+}
